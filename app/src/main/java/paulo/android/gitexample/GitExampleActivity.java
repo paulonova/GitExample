fac3cbc19358a984c7b39aa4d165073e43d8234b
paulonova <paulonova144@gmail.com>
2016-09-15 13:38:59 +0200

    first commit

---
+++ app/src/main/java/paulo/android/gitexample/GitExampleActivity.java
@@ -0,0 +1,13 @@
+package paulo.android.gitexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.git_example);
+    }
+}
